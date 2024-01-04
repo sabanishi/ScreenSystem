58ebe92c03ec33f901fac3eb832f2436c2de9ac0
さばにし <84651801+sabanisi@users.noreply.github.com>
2024-01-04 15:33:22 +0900

    フォルダの場所移動

---
+++ Assets/Plugins/com.sabanishi.screensystem/Runtime/BaseScreen.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace Sabanishi.ScreenSystem
+{
+    public class BaseScreen:MonoBehaviour
+    {
+        
+    }
+}
---
+++ Assets/Plugins/com.sabanishi.screensystem/Runtime/ScreenTransition.cs
@@ -0,0 +1,12 @@
+using Cysharp.Threading.Tasks;
+
+namespace Sabanishi.ScreenSystem
+{
+    public class ScreenTransition
+    {
+        public async UniTask Show()
+        {
+            
+        }
+    }
+}
